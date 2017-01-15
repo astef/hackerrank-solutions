a956798c53995271bff7f9d197ed13ae5fb640e4
astef <a.stefurishin@gmail.com>
2017-01-15 23:33:54 +0300

    begun walk-by-k implementation

--- array-construction/src/array-construction/Solution.cs
+++ array-construction/src/array-construction/Solution.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace array_construction
 {
@@ -8,17 +9,27 @@ namespace array_construction
     {
         public static void Main(string[] args)
         {
-            const string outputFile = "temp/data.txt";
-            File.Delete(outputFile);
-            var n = 0;
-            foreach (var tuple in AnalyzeK(5, 25))
+            //const string outputFile = "temp/data_4_29.txt";
+            //File.Delete(outputFile);
+            //var n = 0;
+            //foreach (var tuple in AnalyzeK(4, 29))
+            //{
+            //    File.AppendAllLines(outputFile, new[]
+            //    {
+            //        $"{n++.ToString().PadRight(7)}" +
+            //        $"{string.Join(" ", tuple.Item1).PadRight(40)}" +
+            //        $"{tuple.Item2.ToString().PadRight(60)}"
+            //    });
+            //    //File.AppendAllLines(outputFile, new[]
+            //    //{
+            //    //    $"{tuple.Item2}"
+            //    //});
+            //}
+
+            var kw = new KWalker();
+            foreach (var arr in kw.WalkByK(13, 19, 34))
             {
-                File.AppendAllLines(outputFile, new[]
-                {
-                    $"{n++.ToString().PadRight(7)}" +
-                    $"{string.Join(" ", tuple.Item1).PadRight(40)}" +
-                    $"{tuple.Item2.ToString().PadRight(60)}"
-                });
+                Console.WriteLine(string.Join(" ", (IEnumerable<string>)arr));
             }
         }
 
@@ -67,6 +78,44 @@ namespace array_construction
                 }
             }
             return false;
+        }
+
+    }
+
+    public class KWalker
+    {
+
+        public IEnumerable<IReadOnlyList<int>> WalkByK(int n, int s, int k)
+        {
+            var arr = new int[n];
+            var coeffs = new int[n].Select((x, i) => (n + 1) % 2 - (n / 2 - i) * 2).ToArray();
+
+            var negas = new int[n - n / 2]; // e<=d<=c<=..
+            var posis = new int[n / 2]; // ..b<=a
+
+            for (var i = negas.Length - 1; i >= 0; i--)
+            {
+                if (!TryIncrementNegas(negas, i, s, n))
+                    continue;
+                for (var j = posis.Length - 1; j >= 0; j--)
+                {
+                    // increment posis
+                }
+            }
+
+
+            throw new NotImplementedException();
+        }
+
+        private static bool TryIncrementNegas(int[] negas, int candidateIndex, int s, int n)
+        {
+            var prefixNegasSum = negas.Take(candidateIndex).Sum();
+            var incrementedValue = negas[candidateIndex] + 1;
+            var incrementedNegasSum = incrementedValue * (negas.Length - candidateIndex);
+            var incrementedPosisSum = (n / 2) * incrementedValue;
+
+            throw new NotImplementedException();
+
         }
 
     }
